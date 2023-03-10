99bc6be40788af062c2a58ad1dcce6dd3d165d46
sagar.jaiswal@jumio.com <sagar.jaiswal@jumio.com>
2023-03-10 16:17:53 +0530

    git action test

--- test_onnx.py
+++ test_onnx.py
@@ -1,3 +1,4 @@
+import unittest
 import torch
 import onnxruntime
 import numpy as np
@@ -9,22 +10,6 @@ from torchvision.transforms import InterpolationMode
 def to_numpy(tensor):
     return tensor.detach().cpu().numpy() if tensor.requires_grad else tensor.cpu().numpy()
 
-
-# def preprocess_numpy1(img):
-#     # convert to RGB if needed
-#     if img.mode != "RGB":
-#         img = img.convert("RGB")
-#     resize = transforms.Resize((224, 224))  # must same as here
-#     crop = transforms.CenterCrop((224, 224))
-#     to_tensor = transforms.ToTensor()
-#     # divide by 255
-#     img = to_tensor(img) / 255
-#     normalize = transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])
-#     img = resize(img)
-#     img = crop(img)
-#     img = to_tensor(img)
-#     img = normalize(img)
-#     return img
 
 def preprocess_numpy(img):
     if img.mode != 'RGB':
@@ -39,16 +24,22 @@ def preprocess_numpy(img):
     return img
 
 
+class TestOnnxModel(unittest.TestCase):
 
-ort_session = onnxruntime.InferenceSession("mtailor.onnx")
-to_tensor = transforms.ToTensor()
+    def setUp(self):
+        self.ort_session = onnxruntime.InferenceSession("mtailor.onnx")
+        self.to_tensor = transforms.ToTensor()
 
-test_data = [("./n01440764_tench.jpeg", 0), ("./n01667114_mud_turtle.JPEG", 35)]
+    def test_model_output(self):
+        test_data = [("./n01440764_tench.jpeg", 0), ("./n01667114_mud_turtle.JPEG", 35)]
+        for img_pth, output in test_data:
+            img = Image.open(img_pth)
+            inp = preprocess_numpy(img).unsqueeze(0)
+            ort_inputs = {self.ort_session.get_inputs()[0].name: to_numpy(inp)}
+            model_output = self.ort_session.run(None, ort_inputs)[0]
+            onnx_res = torch.argmax(self.to_tensor(model_output).unsqueeze_(0))
+            self.assertEqual(onnx_res, output)
 
-for img_pth, output in test_data:
-    img = Image.open(img_pth)
-    inp = preprocess_numpy(img).unsqueeze(0)
-    ort_inputs = {ort_session.get_inputs()[0].name: to_numpy(inp)}
-    model_output = ort_session.run(None, ort_inputs)[0]
-    onnx_res = torch.argmax(to_tensor(model_output).unsqueeze_(0))
-    assert onnx_res==output
+
+if __name__ == '__main__':
+    unittest.main()
